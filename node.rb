19688f39dd160fe00812c8e9c7d2246bef886086
superkmar <kmar.kmar@mail.ru>
2018-06-07 22:28:57 +0300

    Added RDoc comments to Node class, organised some functions, mutation now uses heavy_mutation_cap as a hard limit for applying mutation rate as initially intended

--- node.rb
+++ node.rb
@@ -1,3 +1,8 @@
+# Constants:
+# HIDDEN_NODE_TYPES - an array of node types that hidden nodes can be. Mutation will allow the node types to change inside this array
+# INPUT_NODE_TYPES - a single element array of input node types. In function this type is equivelent to :none.
+# OUTPUT_NODE_TYPES - a single element array of output node types. In function this type is equivelent to :none. Neural network classes return arrays of output node results
+
 HIDDEN_NODE_TYPES = [
 	:sigmoid, 
 	:threshold,
@@ -11,10 +16,39 @@ HIDDEN_NODE_TYPES = [
 INPUT_NODE_TYPES  = [:input]
 OUTPUT_NODE_TYPES = [:output]
 
+# Node class wraps functions for initialization, activation, mutation and cloning
+# 
+# == READERS: 
+# type, id, action
+#
+# == FUNCTIONS
+# 
+# initialize (id, type = :sigmoid, bias = 0, action = :+ ) - creates a node
+# value (sum)
+# activate (x)
+# sigmoid(x)
+# tanh(x)
+# sinh(x)
+# threshold(x)
+# rectified_liniar_unit(x)
+# leaky_rectified_liniar_unit(x)
+# none(x)
+# mutate!
+# clone
+# mutate?
+# mutation_offset
+
 class Node
 
+	#Node readable values:
 	attr_reader :type, :id, :action
 
+	#Creates a new node
+	# === Attributes
+	# * +id+ - used to keep track of nodes, not used in node operation
+	# * +type+ - the activation function to use for return values
+	# * +bias+ - a permanent node input in addition to the node inputs of the value function
+	# * +action+ - the base operation the node uses (addition or multiplication)
 	def initialize(id, type = :sigmoid, bias = 0, action = :+ )
 		@id = id
 		@type = type
@@ -22,7 +56,10 @@ class Node
 		@action = action
 	end
 
-	def value sum = nil
+	#Returns the activation value after applying bias
+	# === Paramater
+	# * +sum+ = +nil+ - the sum or product of all the node inputs. They are added or multiplied with the bias depending on action type
+	def value(sum = nil)
 		if sum.nil?
 			sum = @bias
 		else
@@ -37,6 +74,9 @@ class Node
 		activate sum
 	end
 
+	#Returns the result of applying the activation function according to node +type+
+	# === Paramater
+	# *+x+ - the x value of the function input
 	def activate x
 		case @type
 
@@ -67,44 +107,53 @@ class Node
 	end
 
 	#ACTIVATION FUNCTIONS
+
+	#Returns the result of applying a sigmoid function
 	def sigmoid(x)
 		e = Math::E
 		1.0 / (1 + e**(-x) )
 	end
 
+	#Applies the tanh function
 	def tanh(x)
 		Math.tanh(x)
 	end
 
-	def sinh(x)
-		Math.sinh(x)
-	end
-
+	#returns 1 if x is positive, 0 otherwise. Works in tangent with node bias
 	def threshold(x)
 		x > 0 ? 1.0 : 0.0
 	end
 
+	#ReLu - returns X if it is positive, 0 if not
 	def rectified_liniar_unit(x)
 		x > 0 ? x : 0.0
 	end
 
+	#Divides X by 5 if it's negative, returns X if it's positive
 	def leaky_rectified_liniar_unit(x)
 		x > 0 ? x : (x / 5.0)
 	end
 
+	#Returns X
 	def none(x)
 		x
 	end
 
+	#Returns the Y value of a bell curve function. Maxes out at 1 if X is 0
 	def radial_basis(x)
 		Math::E ** ( - (x**2) )		
 	end
 
 
-
+	#Mutates the node in place
+	#
+	# === Paramaters
+	# *+rate+ - base chance check - used for bias change
+	# *+severity+ - change rate for bias
+	# *+heavy_mutation_cap+ - mutation limit after which node can change action and type
 	def mutate!(rate, severity, heavy_mutation_cap)
 
-		if mutate?(heavy_mutation_cap)
+		if rate > heavy_mutation_cap and mutate?(rate)
 			hidden_nodes = HIDDEN_NODE_TYPES
 			if hidden_nodes.include? @type
 				@type = hidden_nodes.shuffle.first
@@ -112,7 +161,7 @@ class Node
 		end
 
 		#mutate what the node does with the synapses
-		if mutate? (heavy_mutation_cap)
+		if rate > heavy_mutation_cap and mutate?(rate)
 			action_type = rand(2)
 			case action_type
 			when 0
@@ -127,18 +176,29 @@ class Node
 		return self
 	end
 
+	#Returns a new node with identical paramaters
 	def clone
 		new_node = Node.new(@id, @type, @bias, @action)
 	end
 
+	#Checks if mutation should occur
+	#
+	# === Params
+	#
+	# *+rate+ - value from 0 to 1 for mutation chance
 	def mutate? (rate)
 		rand() < rate
 	end
 
+	#Returns a new value near the base value shifted in either direction
+	#
+	# === Params
+	#
+	# *+x+ - base value, new value will be close to this param
+	# *+severity+ - max distance new value can be placed away form base value
 	def mutation_offset x, severity
 		min = (x-severity)
 		max = (x+severity)
 		rand() * (max - min) + min
 	end
-
 end
